8df172794e14042d14a2a5766b0e92787e38086f
elfadili-ae <elfadili.ae@gmail.com>
2024-05-27 00:33:39 +0100

    feat: add carousel in the hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,59 @@
+'use client'
+import Hero from "@/components/Hero";
+import Latest from "@/components/Latest";
 import Navbar from "@/components/Navbar";
+import NewPhones from "@/components/NewPhones";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [menu, setMenu] = useState(false);
+  const [isDown, setIsDown] = useState(false)
+  const menuHandler = (val) => {
+    setMenu(val);
+  }
+
+  useEffect(() => {
+    const handleMenubutton = () => {
+      if (window.scrollY > 30) {
+        setIsDown(true);
+      } else {
+        setIsDown(false);
+      }
+      console.log(window.scrollY)
+    }
+    document.addEventListener('scroll', handleMenubutton);
+
+    //disable page scroll when menu is up
+    if (menu) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+
+    return () => {
+      document.removeEventListener('scroll', handleMenubutton);
+      document.body.classList.remove('no-scroll');
+    }
+  }, [menu])
+
   return (
-    <main className="w-full min-h-screen bg-black">
-      <Navbar />
+    <main className="w-full mx-auto max-w-[1920px] min-h-screen bg-black relative">
+      <div className='w-11 h-5 absolute top-5 left-6 z-20'>
+        <Image className='cursor-pointer' src='/awal_logo_white.png' alt='AwalTheck logo' width={44} height={21} />
+      </div>
+      <div
+        onClick={() => { menuHandler(true) }}
+        className={`fixed top-4 ${isDown && 'bg-stone-700'} z-20 right-6 w-11 h-11 flex flex-col justify-center items-center group ${menu && 'hidden'} hover:bg-white transition-all duration-500 gap-[6px] rounded-full cursor-pointer`}>
+        <div className={`w-6 h-[2px] bg-white group-hover:bg-black transition-all duration-500`}></div>
+        <div className={`w-6 h-[2px] bg-white group-hover:bg-black transition-all duration-500`}></div>
+        <div className={`w-6 h-[2px] bg-white group-hover:bg-black transition-all duration-500`}></div>
+      </div>
+      {menu && <Navbar menuHandler={menuHandler} />}
+      <Hero />
+      <Latest />
+      <NewPhones />
+      <div className="w-full h-screen"></div>
     </main>
   );
 }
