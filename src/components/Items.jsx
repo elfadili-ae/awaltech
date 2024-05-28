2d77335573f932ad40ec71bc4c8cccbb9e320a1e
elfadili-ae <elfadili.ae@gmail.com>
2024-05-28 14:02:05 +0100

    style: animate title and products

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import phonesData from '../data/phones.json'
 import OthersData from '../data/others.json'
 import Image from 'next/image'
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 
+gsap.registerPlugin(ScrollTrigger)
 
 const Items = () => {
+    const gridRef = useRef();
+
+    useGSAP(() => {
+        const gridItems = gsap.utils.toArray(gridRef.current.children);
+
+        gridItems.forEach((item) => {
+            gsap.to(item, {
+                delay: 0.5,
+                duration: 0.5,
+                opacity: 1,
+                rotate: 360,
+                scrollTrigger: item,
+            })
+        })
+    }, [])
     return (
-        <div className='w-full mt-9 grid grid-row grid-cols-2 md:grid-cols-3 lg:grid-cols-4 border-b-[1px] border-slate-400'>
+        <div ref={gridRef} className='w-full mt-9 grid grid-row grid-cols-2 md:grid-cols-3 lg:grid-cols-4 border-b-[1px] border-slate-400'>
             {phonesData.Phones.map((element, index) => {
-                return <div key={index} className='relative text-center w-full aspect-square hover:bg-blue-900 hover:text-white transition-all duration-1000'>
+                return <div key={index} className='opacity-0 relative text-center w-full aspect-square hover:bg-blue-900 hover:text-white transition-all duration-1000'>
                     <Image
                         fill
                         className='object-contain'
@@ -19,7 +38,7 @@ const Items = () => {
                 </div>
             })}
             {OthersData.Others.map((element, index) => {
-                return <div key={index} className='relative text-center w-full aspect-square hover:bg-blue-900 hover:text-white transition-all duration-1000'>
+                return <div key={index} className='opacity-0 relative text-center w-full aspect-square hover:bg-blue-900 hover:text-white transition-all duration-1000'>
                     <Image
                         fill
                         className='object-contain'
--- src/components/NewPhones.jsx
+++ src/components/NewPhones.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Items from './Items'
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap'
+import { ScrollToPlugin } from 'gsap/all'
+
+gsap.registerPlugin(ScrollToPlugin)
 
 const NewPhones = () => {
+
+    useGSAP(() => {
+        gsap.from('.title', {
+            x: -200,
+            opacity: 0,
+            scrollTrigger: '.title',
+        })
+    })
     return (
         <section id='new-phones' className='w-full bg-white pt-8 pb-2 max-w-[1920px]'>
-            <h1 className='ml-5 font-semibold text-2xl md:text-4xl'>Top New Awal Phones</h1>
+            <h1 className='title ml-5 font-semibold text-2xl md:text-4xl'>Top New Awal Phones</h1>
             <Items />
         </section>
     )
